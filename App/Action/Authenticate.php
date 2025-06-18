a2f9d9fc0be9fefe968c7a4f7be380aab0481a91
Kait <git@kait.email>
2025-06-18 00:10:18 -0700

    update password

--- App/Action/Authenticate.php
+++ App/Action/Authenticate.php
@@ -8,7 +8,7 @@ class Authenticate
 {
     public static function handle(string $password): void
     {
-        if (md5($password) === $_ENV['password']) {
+        if (md5($password) === $_ENV['PASSWORD']) {
             $_SESSION['authenticated'] = true;
         } else {
             $_SESSION['error'] = 'Invalid password';
